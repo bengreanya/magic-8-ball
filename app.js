62f02b2dbcce9c7b654876d7f0830e617bb4d35b
Ben Greanya <bengreanya@gmail.com>
2022-10-27 16:34:18 -0700

    added some jscript

--- app.js
+++ app.js
@@ -9,3 +9,31 @@
 /* Display Functions */
 
 // (don't forget to call any display functions you want to run on page load!)
+const submitBtn = document.getElementById('submit');
+const resetBtn = document.getElementbyId('reset');
+const promptSection = document.getElementById('prompt');
+const fortuneSection = document.getElementById('fortune');
+const answerP = document.getElementById('answer');
+
+function doSomething() {}
+const answers = [
+    'Yes, definitely',
+    'It is certain',
+    'It is decidedly so',
+    'Without a doubt',
+    'You may rely on it',
+    'As I see it, yes',
+    'Most Likely',
+    'Outlook good',
+    'Signs point to yes',
+    'Reply hazy, try again',
+    'Ask again later',
+    'Better not tell you now',
+    'Cannot predict now',
+    'Concentrate and ask again',
+    'Don’t count on it',
+    'My reply is no',
+    'My sources say no',
+    'Outlook not so good',
+    'Very doubtful',
+];
